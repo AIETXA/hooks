0ec0708294c7d0e788c9290ca4f680aa22a762b1
Ailén <ailenechaguibel@gmail.com>
2025-06-30 01:33:34 +0200

    no me sale

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css';
+import useCustomHook from './hooks/useCustomHook'
 
 function App() {
   const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
-  const urlRick = 'https://rickandmortyapi.com/api/character/1';
+  const { data, loading, error } = useCustomHook(urlPokemon)
+
+  if(loading) return <p>Cargando...</p>
+  if(error) return <p style={{color:'red'}}>{error}</p>;
+
+
+  //const urlRick = 'https://rickandmortyapi.com/api/character/1';
   
   return (
     <>
+    <h2>Personaje Pokemon</h2>
+    <p>{data.name}</p>
+    <img src={data.sprites?.front_default} alt={data.name} />
     </>
   );
 }
